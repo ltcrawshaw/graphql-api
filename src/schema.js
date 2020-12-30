3d62e1227e1edcf05b7a6aeae0e5599f97e4604f
ltcrawshaw <ltcrawshaw@gmail.com>
2020-12-29 17:20:59 -0800

    populate dynamoDB

--- src/schema.js
+++ src/schema.js
@@ -1,39 +1,14 @@
-const schema = 
+const schema = `
 
-type ListingType {
-  name: String
-}
 
-type ListingActivities {
-  name: String
-}
 
-type Guide {
-  Name: String
-  Bio: String
-  Avatar: String
-}
-
-type Listing {
-     listingId: String
-     coverPhoto: String
-     listingName: String
-     listingDescription: String
-     listingType: [ListingType]
-     listingLocation: String
-     listingActivities: [ListingActivities]
-     specialType: String
-     specialAmount: Int
-     rating: Int
-     guide: Guide
-     price: String
-     numberOfDays: Int
-
-  }
-
+"""
+A hello world Query
+"""
 type Query {
-    getAllListings: [Listing]
-  }
+     hello: String!
+}
 
+`
 
 export {schema}
